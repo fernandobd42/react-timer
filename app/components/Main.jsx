237030e0552a9453c82a7ed5a13a720018184e72
fernandobd42 <fernandolucasbd@gmail.com>
2017-05-14 02:42:56 +0000

    add nav & bundle

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Navigation from './Navigation.jsx'
 const Main = (props) => {
     return (
       <div>
         <div>
           <div>
+            <Navigation/>
             <p>Main.jsx Rendered</p>
             {props.children}
           </div>
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link, IndexLink} from 'react-router'
+
+let Navigation = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+             <ul className="vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown" >
+                    <li className="menu-text">
+                        React Timer App
+                    </li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link" style={{color: "green"}}>Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link" style={{color: "green"}}>CountDown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <span style={{color: "green"}}>Fernando</span>
+                    </li>
+                </ul>
+            </div>            
+        </div>
+    );
+};
+
+module.exports = Navigation;
